0019fc17712762e8d9b65ee932aa02193f4f6cb9
Selim <selim.yenidogan@freelancer.run>
2019-09-19 10:21:33 +0100

    Initial Commit

---
+++ Viper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Viper
+//
+//  Created by Selim KURNAZ on 19.09.2019.
+//  Copyright © 2019 Selim KURNAZ. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
